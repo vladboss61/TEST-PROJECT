29b72982867aa742e18fa1dc3cc2b1b352c3fa29
v.varvashenko <v.varvashenko@HRK1-LHP-F73709>
2018-04-25 10:32:51 +0300

    do

--- UnitTestProject1/UnitTestProject1/UnitTest.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest.cs
@@ -10,6 +10,12 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreNotEqual<int>(1, 2, "Test1");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreNotEqual<int>(1, 1, "Test2");
         }
     }
 }
